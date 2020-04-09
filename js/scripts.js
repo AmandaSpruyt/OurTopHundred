8fac5ad08a9512586ba5cb4aef349d1a8fec1d72
WilliamSpruyt <williamspruyt@hotmail.com>
2020-04-09 22:08:49 +0100

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
  
-var ourFilms=["Yojimbo","In+A+Lonely+Place","The+Thing","Casablanca","Double+Indemnity","Dersu","Seven+Samurai","Bringing+Up+Baby","Twelve+Angry+Men","The+Seventh+Seal","The+Apartment","2001","Winchester+73","High+Noon","Harold+And+Maud"];
+var ourFilms=["Yojimbo","In+A+Lonely+Place","John+Carpenters+The+Thing","Casablanca","Double+Indemnity","Dersu","Bringing+Up+Baby","Twelve+Angry+Men","The+Seventh+Seal",
+"The+Apartment","2001","Winchester+73","High+Noon","Harold+And+Maud","The+Third+Man","Coming+Home","Tokyo+Story",
+"Otto+e+mezzo","Il+Bidone",
+"Modern+Times","Some+Like+It+Hot","Sweet+Smell+Of+Success","Ace+In+THe+Hole","Face+In+The+Crowd","Roma","Sunset+Boulevard","Toni+Erdmann","The+Man+Who+Shot+Liberty+Valance","The+Big+Sleep","Bicycle+Thieves","The+Battle+Of+Algiers", "This+Gun+For+Hire","On+The+Waterfront","Roman+Holiday","Dog+Day+Afternoon","Pickup+On+South+Street","The+Entertainer","Rififi","Il+vangelo+secondo+Matteo","The+Searchers","The+Spy+Who+Came+In+From+The+Cold","Murder,+My+Sweet","Ascenseur+Pour+l'échafaud","Night+Of+The+Hunter","Kind+Hearts+And+Coronets","Rome,+Open+City","La+Règle+Du+Jeu","It+Happened+One+Night","Mr.+Smith+Goes+To+Washington","Out+Of+The+Past","The+Bad+Sleep+Well","Ikiru",
+"Who's+Afraid+Of+Virginia+Woolf?","A+Streetcar+Named+Desire","Ugetsu","Throne+Of+Blood","Wild+Strawberries","African+Queen","Butch+Cassidy+And+The+Sundance+Kid","Harakiri","High+And+Low","La+Strada","Shampoo","The+Circus","Nosferatu","I+Vitelloni","Pickpocket","Zatoichi","Stagecoach","Midnight+Cowboy","The+White+Sheik","The+Shop+Around+The+Corner","Being+There","The+Lives+Of+Others","True+Grit","His+Girl+Friday","City+Lights","Sherlock+Jr." ]
+console.log(ourFilms.length)
+var exceptions=["Seven+Samurai","The+Killing","Out+Of+The+Past","OldBoy","Ran","Late Spring"]
 var BASE_URL = "http://image.tmdb.org/t/p/w185/";
 var API_KEY = "0930f20caa92e0dae95dba06f26b55e4";
 var QUERY="https://api.themoviedb.org/3/search/movie?api_key=fa77b5a712636b454eab3f3147d80d09&query="
@@ -35,27 +41,29 @@ $.getJSON(QUERY+film)
 function update(response) {
 
 res=response.results;
-console.log("num responses "+res.length)
+
 //http://api.themoviedb.org/3/movie/263115?api_key=###&apprend_to_response=videos
 var picIds=[];
 
-replies.push([res[0].id,res[0].overview,res[0].backdrop_path,res[0].poster_path,res[0].id]);
+if(!res[0].poster_path){res[0].poster_path=res[1].poster_path}
+replies.push([res[0].id,res[0].overview,res[0].backdrop_path,res[0].poster_path,res[0].title]);
 
 
  
-var r = new Array(), j = -1;
+var r = new Array();var mods= new Array(), j = -1;
 for (var key=0, size=replies.length; key<size; key++){
-  
- r[++j] ='<div><img src=';
+    
+ r[++j] ='<div data-toggle="modal" data-target="#myModal'+key+'"><img src=';
  r[++j] =BASE_URL+replies[key][3];
 r[++j] =" class=\"posters\" id="
-r[++j] ='\"'+replies[key][0]+'\">';
+r[++j] ='\"'+replies[key][2]+'\">';
  
+
  
  
   
  r[++j] = '</div>';
- 
+ mods.push('<div id="myModal'+key+'" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title"><p>'+replies[key][4]+'</p></h4></div><div class="modal-body"><p>'+replies[key][1]+'</p></div><div class="modal-footer"><button type="button" class="btn btn-default" id='+replies[key][0]+'>Trailer</button></div></div></div></div>')
 
  
 // displayResultCell(BASE_URL+replies[0][3]) ;
@@ -64,6 +72,7 @@ r[++j] ='\"'+replies[key][0]+'\">';
 }
 
 $('#theList').html(r.join(''));
+$('#theModals').html(mods.join(''));
 $('#theList').addClass("animated fadeIn");
 for (var key=0, size=replies.length; key<size; key++){
 picIds.push(document.getElementById(replies[key][0]) );
@@ -71,7 +80,6 @@ $( picIds[key] ).click(function(){
 $.getJSON(MOVIE_TRAILERa+event.target.id+MOVIE_TRAILERb)
 .done(updateTrailer)
 .fail(handleErr);
-
 });
 
 
@@ -94,7 +102,7 @@ function displayResult(poster_path)
 
 $("#topFilm").attr("src",poster_path);
 }function displayResultCell(path)
-{   console.log(path);
+{   
 var firstRow=document.getElementById("theList").rows[0];
 var x=firstRow.insertCell(-1);
 x.innerHTML="New cell";
@@ -117,7 +125,25 @@ function setYear() {
 }
 function updateTrailer(response) {
 resTrailer=response.results;
-console.log(resTrailer);
+ 
 location.href = YOU_TUBE+resTrailer[0].key;
 
+}
+
+function popUp(response) {
+     
+    alert(response)
+     
+    
+    }
+
+function stripPlus(film){
+    spaced=""
+    for(var i=0;i<film.length;i++){
+        if(film.charAt(i)=="+"){
+            spaced+=" ";
+        }
+        else spaced+=film.charAt(i)
+    }
+    returnspaced
 }
